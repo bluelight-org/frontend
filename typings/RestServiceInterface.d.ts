6624bcc6d71e81be5ccb4171bc711b6a2411fda9
Mathis <handymathis@gmail.com>
2021-05-03 20:30:10 +0200

    fixed build

--- typings/RestServiceInterface.d.ts
+++ typings/RestServiceInterface.d.ts
@@ -1,5 +1,9 @@
 // Interface for REST services
 export interface RestServiceInterface {
   login(username: string, password: string): Promise<boolean>;
-  register(username: string, password: string, retypePassword: string): Promise<[boolean, string]>;
+  register(
+    username: string,
+    password: string,
+    retypePassword: string
+  ): Promise<[boolean, string]>;
 }
