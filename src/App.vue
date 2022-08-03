830a75fd191de916fdfec6b46e6e12a93decda38
Mathis <handymathis@gmail.com>
2022-08-03 22:06:31 +0200

    [TASK] Added .env configurations for the application

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <!-- router outlet component -->
     <router-view></router-view>
     <notifications group="notification" position="bottom right" />
-    <ThemeToggler></ThemeToggler>
+    <ThemeToggler v-if="themeTogglerEnabled"></ThemeToggler>
   </div>
 </template>
 
@@ -16,6 +16,12 @@ import ThemeToggler from "@/components/ThemeToggler.vue";
 export default {
   name: "app",
   components: { ThemeToggler },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  data() {
+    return {
+      themeTogglerEnabled: process.env.VUE_APP_THEME_TOGGLER === "enabled"
+    };
+  },
   created(): void {
     // set color scheme
     const colorScheme = getColorScheme();
--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -6,12 +6,30 @@ import { Darkmode } from "@/static/ColorThemes/Darkmode";
 // from the local storage and returns it
 // to the end user
 export function getColorScheme(): ColorScheme {
+  if (process.env.VUE_APP_THEME_TOGGLER !== "enabled")
+    return getTheme(process.env.VUE_APP_DEFAULT_THEME);
+
   const scheme = localStorage.getItem("color-scheme");
   if (scheme != null) {
-    if (scheme == "dark") return new Darkmode();
-    else return new Lightmode();
+    return getTheme(scheme);
   } else {
-    localStorage.setItem("color-scheme", "dark");
-    return new Darkmode();
+    localStorage.setItem("color-scheme", process.env.VUE_APP_DEFAULT_THEME);
+    return getColorScheme();
   }
 }
+
+/**
+ * Gets a specific theme by its name
+ *
+ * @param name The name of the theme
+ */
+const getTheme = (name: string): ColorScheme => {
+  switch (name) {
+    case "dark":
+      return new Darkmode();
+    case "light":
+      return new Lightmode();
+    default:
+      return new Darkmode();
+  }
+};
