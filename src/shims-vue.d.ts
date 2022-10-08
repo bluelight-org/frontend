debe015feec344326251643b9c4d26f9a2269ff9
Mathis <handymathis@gmail.com>
2022-10-08 20:09:11 +0200

    [TASK] Fixed shims types

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,18 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+
+declare module "vue-router" {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  export const install: PluginFunction<{}>;
+}
+
+declare module "vue-resource" {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  export const install: PluginFunction<{}>;
+}
+
+declare module "vue-i18n" {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  export const install: PluginFunction<{}>;
+}
