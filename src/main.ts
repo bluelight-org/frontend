70aa129c5244cd97002b6c91b8514e52610c36a5
Mathis <handymathis@gmail.com>
2022-10-08 20:06:24 +0200

    [TASK] Updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import Bootstrap from "bootstrap-vue";
 import Router from "vue-router";
@@ -6,22 +6,15 @@ import VueResource from "vue-resource";
 import Notifications from "vue-notification";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { initIcons } from "@/font-awsome";
-import VueI18n from "vue-i18n";
 import TranslationHandler from "@/services/TranslationHandler";
-
-Vue.use(Bootstrap);
-Vue.use(Router);
-Vue.use(VueResource);
-Vue.use(Notifications);
-Vue.use(VueI18n);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 initIcons();
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App),
-  i18n: TranslationHandler.getTranslationConfiguration()
-}).$mount("#app");
+createApp(App)
+  .use(Bootstrap)
+  .use(Router)
+  .use(VueResource)
+  .use(Notifications)
+  .use(TranslationHandler.getTranslationConfiguration())
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
