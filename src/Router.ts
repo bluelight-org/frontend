beefb27ade8355cba7a732a921f0617eae4087bf
Mathis <handymathis@gmail.com>
2022-10-08 20:22:56 +0200

    [TASK] Fixed packages

--- src/Router.ts
+++ src/Router.ts
@@ -1,4 +1,9 @@
-import Router from "vue-router";
+import {
+  createRouter,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  createWebHistory,
+} from "vue-router";
 
 // These imports are all routes
 // registered for the router
@@ -9,74 +14,81 @@ import Dashboard from "./routes/Dashboard.vue";
 import Alert from "./routes/Alert.vue";
 import Settings from "./routes/Settings.vue";
 
-const router = new Router({
-  mode: "history",
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/alert",
+    name: "Alert",
+    component: Alert,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  mode: createWebHistory(),
   base: process.env.BASE_URL,
-
-  routes: [
-    {
-      path: "/login",
-      name: "Login",
-      component: Login
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: Register
-    },
-    {
-      path: "/dashboard",
-      name: "Dashboard",
-      component: Dashboard,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
-      path: "/",
-      name: "Dashboard",
-      component: Dashboard,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
-      path: "/alert",
-      name: "Alert",
-      component: Alert,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
-      path: "/settings",
-      name: "Settings",
-      component: Settings,
-      meta: {
-        requiresAuth: true
-      }
-    },
-    {
-      path: "*",
-      name: "NotFound",
-      component: NotFound
-    }
-  ]
+  routes,
 });
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem("accessToken")) {
-      next();
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    if (to.matched.some((record: any) => record.meta.requiresAuth)) {
+      if (localStorage.getItem("accessToken")) {
+        next();
+      } else {
+        next({
+          params: { nextUrl: to.fullPath },
+          name: "Login",
+        });
+      }
     } else {
-      next({
-        params: { nextUrl: to.fullPath },
-        name: "Login"
-      });
+      next();
     }
-  } else {
-    next();
   }
-});
+);
 
 export default router;
